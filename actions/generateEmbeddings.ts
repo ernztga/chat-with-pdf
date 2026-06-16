3ef31fd6db6451233d2decfe32201341479e2b55
Ernz Arellano <ernza@regalcredit.com>
2026-06-16 16:26:56 +0800

    implement ai embeddings

---
+++ actions/generateEmbeddings.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { generateEmbeddingsInPineconeVectorStore } from "@/lib/langchain";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export async function generateEmbeddings(docId: string) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  // Turn a PDF into embeddings
+  await generateEmbeddingsInPineconeVectorStore(docId);
+
+  revalidatePath("/dashboard");
+
+  return { completed: true };
+}
---
+++ app/dashboard/files/[id]/page.tsx
@@ -0,0 +1,5 @@
+function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
+  return <div>ChatToFilepage: {id}</div>;
+}
+
+export default ChatToFilePage;
