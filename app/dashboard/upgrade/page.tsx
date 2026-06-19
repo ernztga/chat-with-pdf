9ad48a3a2036a9b8a1276fe09a2e3c213a88e141
Ernz Arellano <ernza@regalcredit.com>
2026-06-19 10:01:25 +0800

    create initial pricing page

---
+++ app/dashboard/upgrade/page.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/nextjs";
+import { CheckIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+function PricingPage() {
+  const { user } = useUser();
+  const router = useRouter();
+  // pull in user's subscription
+  
+  return (
+    <div>
+      <div className="py-24 sm:py-32">
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">
+            Pricing
+          </h2>
+          <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Supercharge your document companion
+          </p>
+        </div>
+
+        <p className="mx-auto mt-6 max-w-2xl px-10 text-center text-lg leading-8 text-gray-600">
+          Choose an affordable plan that's packed with the best features for
+          interacting with your PDFs, enhancing productivity, and streamlining
+          your workflow
+        </p>
+
+        <div className="max-w-md mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 md:max-w-2xl gap-8">
+          {/* Free */}
+          <div className="ring-1 ring-gray-200 p-8 h-fit pb-12 rounded-3xl">
+            <h3 className="text-lg font-semibold leading-8 text-gray-900">
+              Starter Plan
+            </h3>
+            <p className="mt-4 text-sm leading-6 text-gray-600">
+              Explore Core Features at No Cost
+            </p>
+            <p className="mt-6 flex items-baseline gap-x-l">
+              <span className="text-4xl font-bold tracking-tight text-gray-900">
+                Free
+              </span>
+            </p>
+
+            <ul
+              role="list"
+              className="mt-8 space-y-3 text-sm leading-6 text-gray-600"
+            >
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />2
+                Documents
+              </li>
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                Up to 3 messages per document
+              </li>
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                Try out the AI Chat Functionality
+              </li>
+            </ul>
+          </div>
+
+          {/* Pro */}
+          <div className="ring-2 ring-indigo-600 rounded-3xl p-8">
+            <h3 className="text-lg font-semibold leading-8 text-indigo -600">
+              Pro Plan
+            </h3>
+            <p className="mt-4 text-sm leading-6 text-gray-600">
+              Maximize Productivity with PRO features
+            </p>
+            <p className="mt-6 items-baseline flex gap-x-1">
+              <span className="text-4xl font-bold tracking-tight text-gray-900">
+                $5.99
+              </span>
+              <span className="text-sm font-semibold leading-6 text-gray-600">
+                / month
+              </span>
+            </p>
+
+            <Button className="flex items-center bg-indigo-600 w-full text-white shadow-sm hover:bg-indigo-500 mt-6 rounded-md py-5 px-3 text-sm font-semibold  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Upgrade to Pro
+            </Button>
+
+            <ul
+              role="list"
+              className="mt-8 space-y-3 text-sm leading-6 text-gray-600"
+            >
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                Store up to 20 Documents
+              </li>
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                Ability to Delete Documents
+              </li>
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                Up to 100 messages per document
+              </li>
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                Full Power AI Chat Functionality with Memory Recall
+              </li>
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                Advanced Analytics
+              </li>
+              <li className="flex gap-x-3">
+                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                24-hour support response time
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PricingPage;
